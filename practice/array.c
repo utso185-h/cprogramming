790d21bdc373b507d2b2fc3d85b42d986603e7a7
Utso Roy <85325828+utso185-h@users.noreply.github.com>
2023-05-11 13:26:55 +0600

    Add files via upload

---
+++ practice/array.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+int main()
+{
+    int item[10],i,j;
+    for(i = 0; i<10; i++)
+    {
+        scanf("%d",item[i]);
+
+    }
+    for(i = 9; i>=0; i--)
+    {
+
+        }
+    }
+                printf("%d",item[i]);
+
+
+    return 0;
+}
